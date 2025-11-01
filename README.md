978ea25d41f3ed5a53b845b5274e2cfbd62529a4
as87261491-boop <as87261491@gmail.com>
2025-11-01 22:16:22 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1 align="center">
+  ✨ PROF. DRM Bot ✨
+</h1>
+---
+
+> 🔐 **Note:** CP DRM supported — Minimum quality **360p**  
+> 🚫 **Do not remove the credit tag**
+
+---
+
+## 📜 Commands
+
+| Command | Description |
+|---------|-------------|
+| `/start` | Start the bot |
+| `/stop`  | Stop the bot |
+| `/help`  | Show help message |
+
+---
+
+## 🚀 Deployment Process
+
+### Step-by-Step:
+1. **Fork** the Repository  
+2. **Edit `vars.py`** — Fill in your `API_ID` and `API_HASH`  
+
+---
+
+## 📦 Deploy via Buttons
+
+
+)
---
+++ vars.py
@@ -0,0 +1,24 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "22471119"))
+API_HASH = environ.get("API_HASH", "b8ab98fab8020ad7eab4c38f1ec9e1ae")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8147560840:AAEu7GwPybzPHb9Pgcx-hb7-rwpSGvoCcyg")
+
+OWNER = int(environ.get("OWNER", "5504044604"))
+CREDIT = environ.get("CREDIT", "PROF.")
+
+TOTAL_USER = os.environ.get('TOTAL_USERS', '5504044604').split(',')
+TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
+
+AUTH_USER = os.environ.get('AUTH_USERS', '5504044604').split(',')
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+if int(OWNER) not in AUTH_USERS:
+    AUTH_USERS.append(int(OWNER))
+  
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
+
